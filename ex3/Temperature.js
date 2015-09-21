e6f6a8ce00718e934a0fc772480a77833d29483a
mkondcptr335 <tmiacadet@gmail.com>
2015-09-21 01:28:35 +0000

    pushing temperature file

---
+++ ex3/Temperature.js
@@ -0,0 +1,20 @@
+// Introduction Assignment excersize 3: Temperature 
+
+var tempToday = 82;
+var Temperature = 
+     toFahrenheit = function toFahrenheit(tempinCelsius) 
+    {
+        var tempinFahrenheit = (tempinCelsius *(9/5)) +32);
+                Math.round(tempinFahrenheit);
+    }, 
+    toCelsius = function toCelsius(temptoday) 
+    {
+        var tempinCelsius = (tempToday -32) * (5/9);
+                Math.round(tempinCelsius);
+    } 
+   
+var message = ( "Today's temperature is" tempToday" F, which is" tempinCelsius" C");
+
+
+       
+                console.log(message)
